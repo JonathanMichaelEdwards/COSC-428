b55eaad7f7cbd1999f1313d7885bff765e9959da
Jonathan <jme161@uclive.ac.nz>
2021-03-24 22:17:21 +1300

    updates
    Former-commit-id: a852940134af995db57b74eb3255751943ea7e05

---
+++ Project/Jme161 - Unpiloted Delivery Drone/src/flight_control.py
@@ -0,0 +1,24 @@
+
+# Controlling speed
+
+from djitellopy import tello
+from threading import Thread
+
+from time import sleep
+import cv2, math
+
+
+# me.takeoff()
+
+# me.send_rc_control(0, 50, 0, 0)
+
+# sleep(2)
+
+# me.send_rc_control(0, -50, 0, 90)
+
+# sleep(1)
+
+# me.send_rc_control(0, 0, 0, 0)
+# sleep(3)
+
+# me.land()
