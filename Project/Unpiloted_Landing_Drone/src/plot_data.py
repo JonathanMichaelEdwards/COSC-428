a624e92fbe723aa37cd1599da8e72cab59c1072b
Jonathan <jme161@uclive.ac.nz>
2021-05-03 08:29:44 +1200

    update plotting
    Former-commit-id: 2013f7e6c701bccd74fe88b0d505d697be3b26f1

--- Project/Unpiloted_Landing_Drone/src/plot_data.py
+++ Project/Unpiloted_Landing_Drone/src/plot_data.py
@@ -1,3 +1,5 @@
+#!python3.8
+
 import matplotlib.pyplot as plt
 import FileIO
 import time, csv
@@ -11,8 +13,14 @@ import numpy as np
 # x_data = np.arange(int(float(x_data[0].strip())), int(float(x_data[-2].strip())), 0.5)
 
 plt.plot(x_data, y_data)
-plt.xticks(range(1, 30, 2))
-plt.xlim(0, 21)
+# print(int(float(x_data[0])), int(float(x_data[-1])))
+# xi = list(range(int(float(x_data[0])), int(float(x_data[-1]))))
+# print(np.size(xi), np.size(x_data))
+# print(x_data[-1])
+# plt.xticks(x_data, xi)
+plt.axis([min(x_data), max(x_data), min(y_data), max(y_data)])
+plt.xticks(np.arange(-1, 20, 5))
+plt.xlim(-1, 20)
 plt.xlabel('Time (s)')
 plt.ylabel('Yaw Angle (∠)')
 plt.title('Yaw Control')
