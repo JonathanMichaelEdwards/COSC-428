771d6edc9441d8847761bd3b04ede435361a0d86
Jonathan <jme161@uclive.ac.nz>
2021-05-04 00:38:58 +1200

    Control updates
    Former-commit-id: 87bd4d17e88ad1b742b0b80242b709f0527abc05 [formerly 87bd4d17e88ad1b742b0b80242b709f0527abc05 [formerly df152518846c08e3c7beddd4e838a21a8ebb8956]]
    Former-commit-id: 5f8ade09abe5949b73a33035cec7b4f0f1a2fbbd
    Former-commit-id: bb4967feaec5ca1a137b3090dcfc40153f8aabc7

--- Project/Unpiloted_Landing_Drone/src/FileIO.py
+++ Project/Unpiloted_Landing_Drone/src/FileIO.py
@@ -11,7 +11,10 @@ def write(x_data, y_data, fileName):
     with open(f"{fileName}.csv",'w') as file:
         write = csv.writer(file, delimiter=',', lineterminator='\n')
         while i < len(x_data):  # Seperate data
-            write.writerow([x_data[i], y_data[i]])
+            try:
+                write.writerow([x_data[i], y_data[i]])
+            except IndexError:
+                pass
             i += 1
     file.close()
 
@@ -36,11 +39,3 @@ def read(file):
     file.close()
     
     return (buff_x, buff_y)
-
-
-# x = [1.11, 2.22, 3.55]
-# y = [4, 5, 6]
-# write(x, y, "src/data/yaw_plotting_data")
-# (x_data, y_data) = read("src/data/yaw_plotting_data")
-# if (
-# print(x_data, x_data)
