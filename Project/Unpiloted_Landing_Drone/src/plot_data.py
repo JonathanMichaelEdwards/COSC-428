87bd4d17e88ad1b742b0b80242b709f0527abc05
Jonathan <jme161@uclive.ac.nz>
2021-05-04 00:38:58 +1200

    Control updates
    Former-commit-id: df152518846c08e3c7beddd4e838a21a8ebb8956

--- Project/Unpiloted_Landing_Drone/src/plot_data.py
+++ Project/Unpiloted_Landing_Drone/src/plot_data.py
@@ -7,24 +7,14 @@ import numpy as np
 
 
 (x_data, y_data) = FileIO.read("src/data/yaw_plotting_data")
-# _x_data = list(range(len(x_data)))
-# plt.xticks(_x_data, x_data)
-# print(int(float(x_data[-2].strip())))
-# x_data = np.arange(int(float(x_data[0].strip())), int(float(x_data[-2].strip())), 0.5)
 
 plt.plot(x_data, y_data)
-# print(int(float(x_data[0])), int(float(x_data[-1])))
-# xi = list(range(int(float(x_data[0])), int(float(x_data[-1]))))
-# print(np.size(xi), np.size(x_data))
-# print(x_data[-1])
-# plt.xticks(x_data, xi)
 plt.axis([min(x_data), max(x_data), min(y_data), max(y_data)])
-plt.xticks(np.arange(-1, 20, 5))
+plt.xticks(np.arange(-1, 20, 2))
 plt.xlim(-1, 20)
 plt.xlabel('Time (s)')
 plt.ylabel('Yaw Angle (∠)')
 plt.title('Yaw Control')
 # plt.savefig('figure1.png')
 plt.show()
-
 
